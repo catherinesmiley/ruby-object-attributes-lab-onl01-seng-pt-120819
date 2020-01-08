1fa22e03df246dd85c525abc8fd6f48d6d306ca0
Catherine Smiley <catherinesmiley@gmail.com>
2020-01-08 19:10:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,54 @@
+# class Dog 
+#   def name=(dog_name)
+#     @name = dog_name
+#   end
+  
+#   def name 
+#     @name
+#   end
+  
+#   def breed=(dog_breed)
+#     @breed = dog_breed
+#   end
+  
+#   def breed
+#     @breed
+#   end
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Dog 
-  def name=(dog_name)
-    @name = dog_name
+  
+  def name=(name)
+    @name = name
   end
   
-  def name 
+  def name
     @name
   end
   
-  def breed=(dog_breed)
-    @breed = dog_breed
+  def breed=(breed)
+    @breed = breed
   end
   
   def breed
     @breed
   end
+  
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,17 +1,53 @@
-class Person
-  def name=(person_name)
-    @name = person_name
+# class Person
+#   def name=(person_name)
+#     @name = person_name
+#   end
+  
+#   def name
+#     @name
+#   end
+  
+#   def job=(person_job)
+#     @job = person_job
+#   end
+  
+#   def job
+#     @job
+#   end
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Person 
+  
+  def name=(name)
+    @name = name
   end
   
-  def name
+  def name 
     @name
   end
   
-  def job=(person_job)
-    @job = person_job
+  def job=(job)
+    @job = job
   end
   
-  def job
-    @job
+  def job 
+    @job 
   end
+  
 end
